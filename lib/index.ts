e5a64428914775f66e3e90b4c119e5ab4c4d8514
Nikolaos Tsompanidis <nikos.ts@causaly.com>
2022-10-02 10:06:07 +0300

    feat: export first batch functions

--- lib/index.ts
+++ lib/index.ts
@@ -1,1 +1,8 @@
-export { sum } from './sum';
+export { groupBy } from './groupBy';
+export { intersect } from './intersect';
+export { intersectWith } from './intersectWith';
+export { isDistinctArray } from './isDistinctArray';
+export { isSubsetOf } from './isSubsetOf';
+export { isSubsetOfWith } from './isSubsetOfWith';
+export { unique } from './unique';
+export { uniqueBy } from './uniqueBy';
