bd65452bdb05be0ba88e490d98e8bac43f401895
Dimitrios C. Michalakos <dimitris@jmike.gr>
2023-04-21 10:42:01 +0300

    test: add test for using unique with fp-ts pipe

--- lib/unique/unique.test.ts
+++ lib/unique/unique.test.ts
@@ -1,21 +1,26 @@
-import { unique } from './unique';
+import { pipe } from 'fp-ts/lib/function';
+import { expectType } from 'ts-expect';
 
-const mockData = [
-  'Greece',
-  'Greece',
-  'Greece',
-  'United States',
-  'United Kingdom',
-  'United States',
-  'Greece',
-  'Greece',
-  'United States',
-  'United States',
-];
+import { NonEmptyArray, NonEmptyArrayAlt } from '../types';
+import { unique } from './unique';
 
 describe('unique()', () => {
   it('returns unique values', () => {
+    const mockData = [
+      'Greece',
+      'Greece',
+      'Greece',
+      'United States',
+      'United Kingdom',
+      'United States',
+      'Greece',
+      'Greece',
+      'United States',
+      'United States',
+    ];
+
     const expected = unique(mockData);
+    expectType<Array<string>>(expected);
 
     expect(expected).toMatchInlineSnapshot(`
       [
@@ -24,5 +29,33 @@ describe('unique()', () => {
         "United Kingdom",
       ]
     `);
+  });
+
+  it('accepts NonEmptyArray', () => {
+    const mockData: NonEmptyArray<number> = [1, 2, 2, 3, 3, 3];
+
+    const expected = unique(mockData);
+    expectType<NonEmptyArray<number>>(expected);
+
+    expect(expected).toEqual([1, 2, 3]);
+  });
+
+  it('accepts NonEmptyArrayAlt', () => {
+    const mockData: NonEmptyArrayAlt<number> = [1, 2, 2, 3, 3, 3];
+
+    const expected = unique(mockData);
+    expectType<NonEmptyArrayAlt<number>>(expected);
+
+    expect(expected).toEqual([1, 2, 3]);
+  });
+
+  it('works successfully with fp-ts pipe', () => {
+    type Value = 'a' | 'b' | 'c';
+    const mockData: NonEmptyArray<Value> = ['a', 'b', 'b', 'c', 'c', 'c'];
+
+    const expected = pipe(mockData, unique);
+    expectType<NonEmptyArray<Value>>(expected);
+
+    expect(expected).toEqual(['a', 'b', 'c']);
   });
 });
