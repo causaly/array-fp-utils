0d61feb950061fd2e6ef06bca970afa138dcf1e4
Dimitrios C. Michalakos <dimitris@jmike.gr>
2024-07-01 09:43:57 +0300

    feat: extend uniqueBy to handle non-empty arrays

--- lib/uniqueBy/uniqueBy.ts
+++ lib/uniqueBy/uniqueBy.ts
@@ -1,23 +1,46 @@
+import { NonEmptyArray, NonEmptyArrayAlt } from '../types';
+
+export function _uniqueBy<
+  T extends
+    | ReadonlyArray<unknown>
+    | Readonly<NonEmptyArray<unknown>>
+    | Readonly<NonEmptyArrayAlt<unknown>>,
+  K
+>(
+  arr: T,
+  getUniqueKey: (value: T[number]) => K,
+  reduceValue: (prevValue: T[number], nextValue: T[number]) => T[number] = (
+    prevValue
+  ) => prevValue
+): T {
+  const map = new Map<K, T[number]>();
+
+  for (const value of arr) {
+    const key = getUniqueKey(value);
+    const existingValue = map.get(key);
+
+    if (!existingValue) {
+      map.set(key, value);
+    } else {
+      map.set(key, reduceValue(existingValue, value));
+    }
+  }
+
+  return Array.from(map.values()) as T;
+}
+
 export const uniqueBy =
-  <ValueType, KeyType>(
-    getUniqueKey: (value: ValueType) => KeyType,
-    reduceValue: (prevValue: ValueType, nextValue: ValueType) => ValueType = (
+  <
+    T extends
+      | ReadonlyArray<unknown>
+      | Readonly<NonEmptyArray<unknown>>
+      | Readonly<NonEmptyArrayAlt<unknown>>,
+    K
+  >(
+    getUniqueKey: (value: T[number]) => K,
+    reduceValue: (prevValue: T[number], nextValue: T[number]) => T[number] = (
       prevValue
     ) => prevValue
   ) =>
-  (arr: Readonly<Array<ValueType>>): Array<ValueType> => {
-    const map = arr.reduce((acc, value) => {
-      const key = getUniqueKey(value);
-      const existingValue = acc.get(key);
-
-      if (!existingValue) {
-        acc.set(key, value);
-      } else {
-        acc.set(key, reduceValue(existingValue, value));
-      }
-
-      return acc;
-    }, new Map<KeyType, ValueType>());
-
-    return Array.from(map.values());
-  };
+  (arr: T): T =>
+    _uniqueBy(arr, getUniqueKey, reduceValue);
